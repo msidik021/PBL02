264fb4830746a28c1b266b323fa66a21f600652b
msidik021 <muhammadsidik0021@gmail.com>
2025-09-11 16:09:57 +0700

    menambah tugas

---
+++ Design Thinking/Javascript/Basic JS/Tugas/LampuLaluLintas.js
@@ -0,0 +1,22 @@
+const warnaLampu = prompt("Masukkan warna lampu lalu lintas (merah, kuning, atau hijau):");
+const outputElement = document.getElementById("output-status");
+if (warnaLampu === "merah") {
+  // Jika inputnya "merah", ubah konten elemen output.
+  outputElement.innerHTML = "Kendaraan berhenti. Jangan maju nanti ketabrak";
+  // Mengubah warna teks output menjadi merah
+  outputElement.style.color = "red";
+} else if (warnaLampu === "kuning") {
+  // Jika inputnya "kuning", ubah konten elemen output.
+  outputElement.innerHTML = "Kendaraan berhati-hati.";
+  // Mengubah warna teks output menjadi kuning
+  outputElement.style.color = "orange";
+} else if (warnaLampu === "hijau") {
+  // Jika inputnya "hijau", ubah konten elemen output.
+  outputElement.innerHTML = "Kendaraan berjalan. Maju terus";
+  // Mengubah warna teks output menjadi hijau
+  outputElement.style.color = "green";
+} else {
+  // Jika inputnya tidak sesuai, berikan pesan error.
+  outputElement.innerHTML = "Input tidak valid. Mohon masukkan 'merah', 'kuning', atau 'hijau'.";
+  outputElement.style.color = "black";
+}
